4913276986b9479bc5be686f0dceedd7f9b92fe6
Alexander Hickey <81879393+awhic@users.noreply.github.com>
2021-11-14 00:07:38 -0500

    Revert "added standard to metric tab"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,8 +14,7 @@ import { BmiCalculatorFormComponent } from './forms/bmi-calculator-form/bmi-calc
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    MatButtonToggleModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
