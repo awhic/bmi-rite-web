268ebfff154c047ffb5c2896afcb812d584279ae
alex <alexh1199@hotmail.com>
2022-01-02 03:40:13 -0500

    made button and switching method with img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bmi-rite';
   typeSelect = true;
+  themeSelect = true;
 
   public changeType() {
     if (this.typeSelect) {
       this.typeSelect = false;
     } else {
       this.typeSelect = true;
+    }
+  }
+
+  public changeTheme() {
+    if (this.themeSelect) {
+      this.themeSelect = false;
+    } else {
+      this.themeSelect = true;
     }
   }
 }
