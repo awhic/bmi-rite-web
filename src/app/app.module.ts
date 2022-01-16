6aa119228035eacb51d358b3dfa4e4630ea79cbc
alex <alexh1199@hotmail.com>
2022-01-16 16:13:24 -0500

    just want to add ability to download java version in new card below generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +19,8 @@ import { FridaeContentComponent } from './fridae-content/fridae-content.componen
 import { RouterModule } from '@angular/router';
 import { HomeContentComponent } from './home-content/home-content.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { QuikPassContentComponent } from './quik-pass-content/quik-pass-content.component';
+import { QuikPassFormComponent } from './forms/quik-pass-form/quik-pass-form.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BmiCalculatorContentComponent,
     FridaeContentComponent,
     HomeContentComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    QuikPassContentComponent,
+    QuikPassFormComponent
   ],
   imports: [
     BrowserModule,
@@ -34,6 +40,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
       { path: 'bmi', component: BmiCalculatorContentComponent },
       { path: 'fridae', component: FridaeContentComponent },
       { path: 'home', component: HomeContentComponent },
+      { path: 'password', component: QuikPassContentComponent},
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
     ]),
@@ -48,7 +55,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MatSlideToggleModule,
     MatRippleModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
